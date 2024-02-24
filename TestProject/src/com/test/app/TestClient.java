530e311173c99e33742e116428c406b91958c1a5
manda <manda@host.docker.internal>
2024-02-24 09:33:08 -0500

    first commit

---
+++ TestProject/src/com/test/app/TestClient.java
@@ -0,0 +1,11 @@
+package com.test.app;
+
+public class TestClient {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Tes Client is Completed");
+	}
+
+}
